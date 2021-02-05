2ff3e8b5219de21f2f4e9ae389fec9b7103fdce1
01ritzs <01ritzs@gmail.com>
2021-02-06 00:29:19 +0530

    Frist Commit

---
+++ app/src/main/java/com/du/de/rasandummy/HomeActivity.java
@@ -0,0 +1,18 @@
+package com.du.de.rasandummy;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+    }
+}
